8a97cb801cc0c96bb8d7ec8917bad91612a1a9b6
kluwena <karelluwena@gmail.com>
2017-07-29 12:47:06 -0700

    import component and modules to app

---
+++ client/src/components/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { sampleAction } from '../actions';
+import './css/App.css';
