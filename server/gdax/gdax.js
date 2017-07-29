cf9fb293cbe3bc7712a040fdd3dac2be72061f19
Ryan Chow <chowryan@gmail.com>
2017-07-29 15:24:00 -0700

    Working on gdax api.

--- server/gdax/gdax.js
+++ server/gdax/gdax.js
@@ -3,21 +3,24 @@ const getGDXHistoricRates = (productId, start, end, granularity) => {
   productId = productId || 'BTC-USD';
   start = start || '2017-07-21T17:00:00-07:00';
   end = end || '2017-07-28T17:00:00-07:00';
-  granularity = granularity || '86400';
+  granularity = granularity || 86400/2;
   const gdxURL = `https://api-public.sandbox.gdax.com/products/${ productId }/candles?start=${ start }&end=${ end }&granularity=${ granularity }`;
   return axios.get(gdxURL)
   .then(res => {
-    console.log(res.data);
-    return { data: res.data, error: null };
+    // [ time, low, high, open, close, volume ]
+    const filterData = res.data.map(dataPoint => {
+      return { time: dataPoint[0],
+        open: dataPoint[3],
+        close: dataPoint[4],
+      };
+    });
+    console.log(filterData);
+    return { data: filterData, error: null };
   })
   .catch(err => {
     console.log(err);
     return { data: null, error: err };
-  })
-}
- 
+  });
+};
+
 getGDXHistoricRates();
-
-
-
-
